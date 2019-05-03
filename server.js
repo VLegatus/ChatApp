766075c20556b6299877ea0e119d345346e01eb7
PristinePrograms <PristinePrograms@gmail.com>
2019-05-03 15:31:51 -0400

    Bootstrap Added

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ var express = require('express');
 
 var app = express();
 
-app.listen(3000);
+app.use(express.static(__dirname));
+
+var server = app.listen(3000, () => {
+    console.log('Server is listening on port', server.address().port);
+});
