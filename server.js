94fe49f11f5a4576834faab09d7a193c0409d247
PristinePrograms <PristinePrograms@gmail.com>
2019-05-03 18:11:14 -0400

    Database Functionality

--- server.js
+++ server.js
@@ -1,29 +1,53 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
-
-
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
+
+var dbUrl = 'mongodb+srv://user:user@learning-node-fjj20.mongodb.net/test?retryWrites=true';
 
 var messages = [
     {name : 'Tim' , message: 'Hi'},
     {name : 'Jane' , message: 'Hello'}
 ];
 
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+});
+
+
 app.get('/messages', (req, res) =>{
-    res.send(messages);
-})
+    Message.find({}, (err, messages) => {
+        res.send(messages);
+    });
+});
 
 app.post('/messages', (req, res) =>{
-    console.log(req.body);
-    messages.push(req.body);
-    res.sendStatus(200);
-})
+    var message = new Message(req.body);
 
-var server = app.listen(3000, () => {
+    message.save((err) => {
+        if(err){
+            sendStatus(500);
+        }
+        io.emit('message', req.body);
+        res.sendStatus(200);
+    });
+});
+
+io.on('connection', (socket) => {
+    console.log('A User Connected');
+});
+
+mongoose.connect(dbUrl, {useNewUrlParser: true}, (err) => {
+    console.log('mongo db connection', err);
+});
+
+var server = http.listen(3000, () => {
     console.log('Server is listening on port', server.address().port);
 });
