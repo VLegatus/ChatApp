a5e10b54969e68faeb82725bebd2b8987cb40980
PristinePrograms <PristinePrograms@gmail.com>
2019-05-03 16:28:38 -0400

    Messages

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
+
 
 var app = express();
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
 
-app.get('/messsage', (req, res) =>{
-    res.send('hello');
+var messages = [
+    {name : 'Tim' , message: 'Hi'},
+    {name : 'Jane' , message: 'Hello'}
+];
+
+app.get('/messages', (req, res) =>{
+    res.send(messages);
+})
+
+app.post('/messages', (req, res) =>{
+    console.log(req.body);
+    messages.push(req.body);
+    res.sendStatus(200);
 })
 
 var server = app.listen(3000, () => {
